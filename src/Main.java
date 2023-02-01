b8cbd6d529a902a6f223ed15cd00e6cb8f3081af
student <bonniec15@nycstudents.net>
2023-02-01 10:22:54 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+public class Main {
+    public static void main (String [] args) {
+        ArrayList<Integer> ints = new ArrayList<> ();
+        ints.add(10);
+        ints.add(5);
+        ints.add(24);
+        System.out.println(ints);
+    }
 }
